e33cf8692f7189279181395ed70971dc1b388fbf
sajjad0321 <sajjadjaved01@gmail.com>
2017-02-25 01:22:40 +0500

    more code

--- app/src/main/java/com/muparse/MainActivity.java
+++ app/src/main/java/com/muparse/MainActivity.java
@@ -1,96 +1,119 @@
 package com.muparse;
 
-import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.RequiresApi;
-import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v4.widget.SearchViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity{
+import static com.muparse.R.id.channelName;
 
-    private static final File DEFA = Environment.getExternalStorageDirectory();
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+
     TextView mPlaylistParams;
     RecyclerView mPlaylistList;
-    private File dir = new File(DEFA.getAbsolutePath()+"/Netuptv");
+    SessionManagement sess;
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         PlaylistAdapter mAdapter;
 
         InputStream is;
         M3UParser parser = new M3UParser();
 
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        EditText channelName = (EditText) findViewById(R.id.channelName);
+        ListView lv = (ListView) findViewById(R.id.list_view);
+        AutoCompleteTextView mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+
         mPlaylistParams = (TextView) findViewById(R.id.playlist_params);
         mPlaylistList = (RecyclerView) findViewById(R.id.playlist_recycler);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         mPlaylistList.setLayoutManager(layoutManager);
-
         mAdapter = new PlaylistAdapter(this);
         mPlaylistList.setAdapter(mAdapter);
+
         try {
             is = getAssets().open("tv_channels.m3u");
             M3UPlaylist playlist = parser.parseFile(is);
             mPlaylistParams.setText(playlist.getPlaylistParams());
             mAdapter.update(playlist.getPlaylistItems());
-            //Toast.makeText(this, playlist.getSingleParameter("getItemUrl"), Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
     }
 
-//    public void blaClick(View view) {
-//        Intent intent = new Intent(Intent.ACTION_VIEW); //http://portal.onlineiptv.net:5210/get.php?username=000&password=000&type=m3u&output=ts
-//        Uri videoUri = Uri.parse("http://portal.onlineiptv.net:5210/live/000/000/8517.ts");
-//        intent.setDataAndType( videoUri, "application/x-mpegURL" );
-//        intent.setPackage( "com.mxtech.videoplayer.ad" );
-//        startActivity( intent );
-//    }
-boolean installed = false;
-    public boolean isAppInstalled(String uri) {
-        PackageManager pm = getPackageManager();
+    public boolean isPackageInstalled(String packagename, PackageManager packageManager) {
         try {
-            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
-            installed = true;
+            packageManager.getPackageInfo(packagename, PackageManager.GET_ACTIVITIES);
+            return true;
         } catch (PackageManager.NameNotFoundException e) {
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-            installed = false;
+            return false;
         }
-        return installed;
     }
 
-    @SuppressWarnings("All")
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 1: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if (!dir.exists()){
-                        dir.mkdir();
-                    }
-                    //user: X6QXN76met passwd: Jyw2SMYjxe
-//                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://portal.onlineiptv.net:5210/get.php?username=000&password=000&type=m3u&output=ts"));
-//                    startActivity(browserIntent);
-                } else {
-                    Toast.makeText(this, "Permission denied ☻", Toast.LENGTH_SHORT).show();
-                }
-                return;
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.app_bar_search:
+                setContentView(R.layout.searchable);
+                break;
+            case R.id.logout:
+                sess.logoutUser();
+                sess.deleteDirectory(Login.dir.getAbsoluteFile());
+                break;
+            default: {
+                return super.onOptionsItemSelected(item);
             }
-            // other 'case' lines to check for other permissions this app might request
         }
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String query) {
+        // Here is where we are going to implement the filter logic
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
     }
 }
--- app/src/main/java/com/muparse/PlaylistAdapter.java
+++ app/src/main/java/com/muparse/PlaylistAdapter.java
@@ -5,9 +5,15 @@ package com.muparse;
  */
 
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,7 +30,6 @@ public class PlaylistAdapter extends RecyclerView.Adapter<PlaylistAdapter.ItemHo
     private List<M3UItem> mItem = new ArrayList<M3UItem>();
     private Context mContext;
     private LayoutInflater mInflater;
-    private Intent intent = new Intent();
     private MainActivity min = new MainActivity();
 
     public PlaylistAdapter(Context c) {
@@ -64,47 +69,71 @@ public class PlaylistAdapter extends RecyclerView.Adapter<PlaylistAdapter.ItemHo
     public class ItemHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView name;
-        TextView url;
+        //TextView url;
 
         public ItemHolder(View view) {
             super(view);
             view.setOnClickListener(this);
             name = (TextView) view.findViewById(R.id.item_name);
-            url = (TextView) view.findViewById(R.id.item_url);
+            //url = (TextView) view.findViewById(R.id.item_url);
         }
 
         public void update(final M3UItem item) {
             name.setText(item.getItemName());
-            url.setText(item.getItemUrl());
+            // url.setText(item.getItemUrl());
         }
 
-        boolean isApp = min.isAppInstalled("com.mxtech.videoplayer");
+        PackageManager pm = mContext.getPackageManager();
+
+        boolean isApp = min.isPackageInstalled("com.mxtech.videoplayer.ad", pm);
+
         public void onClick(View v){
-            int position = getLayoutPosition();
-            final M3UItem imm = mItem.get(position);
-            if (isApp) {
-                Toast.makeText(mContext, "ApplicationInstalled", Toast.LENGTH_SHORT).show();
-            }else {
-                Toast.makeText(mContext, "App Not Found", Toast.LENGTH_SHORT).show();
+            try {
+                int position = getLayoutPosition();
+                final M3UItem imm = mItem.get(position);
+                if (isApp) { //isApplicationInstalled
+                    playy(imm.getItemUrl(), imm.getItemName());
+                } else { // isApplicationNotFound
+                    playerNotFound();
+                }
+            } catch (Exception e) {
+                Toast.makeText(mContext, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
             }
-//            Toast.makeText(mContext, "Url: "+imm.getItemUrl(), Toast.LENGTH_LONG).show();
-            playy(imm.getItemUrl());
         }
 
-        public void playy(String uriil){
+        void playy(String urli, String channel) {
             try{
-                Uri videoUri = Uri.parse(uriil); //http://portal.onlineiptv.net:5210/live/000/000/8517.ts
-                intent = new Intent(Intent.ACTION_VIEW);
+                Uri videoUri = Uri.parse(urli);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setDataAndType(videoUri, "application/x-mpegURL" );
-//                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 intent.setPackage( "com.mxtech.videoplayer.ad" );
-                mContext.startActivity( intent );
-
+                intent.putExtra("title", channel);
+                mContext.startActivity(intent);
             } catch (Exception e) {
                 Toast.makeText(mContext, "Error: "+e.getMessage(), Toast.LENGTH_LONG).show();
             }
-//            Intent marketIntent = new Intent(Intent.ACTION_VIEW, marketUri);
-//            mContext.startActivity(marketIntent);
+        }
+
+        void playerNotFound() {
+            AlertDialog.Builder localBuilder = new AlertDialog.Builder(mContext);
+            localBuilder.setTitle("iPtv");
+            localBuilder.setMessage("Player not found. Install MX Player.");
+            localBuilder.setPositiveButton("Install", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface paramAnonymousDialogInterface, int paramAnonymousInt) {
+                    try {
+                        mContext.startActivity(new Intent("android.intent.action.VIEW", Uri.parse("market://details?id=com.mxtech.videoplayer.ad")));
+                    } catch (ActivityNotFoundException localActivityNotFoundException) {
+                        mContext.startActivity(new Intent("android.intent.action.VIEW", Uri.parse("https://play.google.com/store/apps/details?id=com.mxtech.videoplayer.ad&hl=en")));
+                    }
+                }
+            });
+//        localBuilder.setNegativeButton("Google", new DialogInterface.OnClickListener()
+//        {
+//            public void onClick(DialogInterface paramAnonymousDialogInterface, int paramAnonymousInt) {}
+//        });
+            //localBuilder.setCancelable(false);
+            localBuilder.create().show();
         }
     }
 }
